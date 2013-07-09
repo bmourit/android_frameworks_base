af44912e4c79fb53284567555edf0b316c88e7db
Boyd <irvinmouritsen@gmail.com>
2013-07-09 02:47:54 -0700

    ATM7029 specific Window Orientation fix
    This fixes the strange behavior with ATM7029 window rotations.

--- services/java/com/android/server/display/LocalDisplayAdapter.java
+++ services/java/com/android/server/display/LocalDisplayAdapter.java
@@ -37,24 +37,42 @@ import java.io.PrintWriter;
  */
 final class LocalDisplayAdapter extends DisplayAdapter {
     private static final String TAG = "LocalDisplayAdapter";
+    private static final int mDefaultRotation;
+    private static final int mHwRotation;
+    private static final int[] BUILT_IN_DISPLAY_IDS_TO_SCAN;
 
-    private static final int[] BUILT_IN_DISPLAY_IDS_TO_SCAN = new int[] {
-            Surface.BUILT_IN_DISPLAY_ID_MAIN,
-            Surface.BUILT_IN_DISPLAY_ID_HDMI,
-    };
-
+    static {
+        int[] ids;
+        ids = new int[2];
+        ids[0] = Surface.BUILT_IN_DISPLAY_ID_MAIN;
+        ids[1] = Surface.BUILT_IN_DISPLAY_ID_HDMI;
+        BUILT_IN_DISPLAY_IDS_TO_SCAN = ids;
+        mDefaultRotation = SystemProperties.getInt("ro.sf.default_rotation", 0);
+        mHwRotation = SystemProperties.getInt("ro.sf.hwrotation", 0) / 90;
+    }
+    
     private final SparseArray<LocalDisplayDevice> mDevices =
             new SparseArray<LocalDisplayDevice>();
     private HotplugDisplayEventReceiver mHotplugReceiver;
 
     private final PhysicalDisplayInfo mTempPhys = new PhysicalDisplayInfo();
-
+        
     // Called with SyncRoot lock held.
     public LocalDisplayAdapter(DisplayManagerService.SyncRoot syncRoot,
             Context context, Handler handler, Listener listener) {
         super(syncRoot, context, handler, listener, TAG);
     }
-
+    
+    static int access$000() {
+        return mHwRotation;
+    }
+    static int access$100() {
+        return mDefaultRotation;
+    }
+    static void access$200(LocalDisplayAdapter r0) {
+        r0.scanDisplaysLocked();
+    }
+    
     @Override
     public void registerLocked() {
         super.registerLocked();
@@ -62,29 +80,29 @@ final class LocalDisplayAdapter extends DisplayAdapter {
         mHotplugReceiver = new HotplugDisplayEventReceiver(getHandler().getLooper());
         scanDisplaysLocked();
     }
-
+    
     private void scanDisplaysLocked() {
         for (int builtInDisplayId : BUILT_IN_DISPLAY_IDS_TO_SCAN) {
             IBinder displayToken = Surface.getBuiltInDisplay(builtInDisplayId);
-            if (displayToken != null && Surface.getDisplayInfo(displayToken, mTempPhys)) {
-                LocalDisplayDevice device = mDevices.get(builtInDisplayId);
-                if (device == null) {
-                    // Display was added.
-                    device = new LocalDisplayDevice(displayToken, builtInDisplayId, mTempPhys);
-                    mDevices.put(builtInDisplayId, device);
-                    sendDisplayDeviceEventLocked(device, DISPLAY_DEVICE_EVENT_ADDED);
-                } else if (device.updatePhysicalDisplayInfoLocked(mTempPhys)) {
-                    // Display properties changed.
-                    sendDisplayDeviceEventLocked(device, DISPLAY_DEVICE_EVENT_CHANGED);
-                }
-            } else {
-                LocalDisplayDevice device = mDevices.get(builtInDisplayId);
-                if (device != null) {
-                    // Display was removed.
-                    mDevices.remove(builtInDisplayId);
-                    sendDisplayDeviceEventLocked(device, DISPLAY_DEVICE_EVENT_REMOVED);
-                }
-            }
+        	if (displayToken != null && Surface.getDisplayInfo(displayToken, mTempPhys)) {
+        		LocalDisplayDevice device = mDevices.get(builtInDisplayId);
+        		if (device == null) {
+        			// Display was added.
+        			device = new LocalDisplayDevice(displayToken, builtInDisplayId, mTempPhys);
+        			mDevices.put(builtInDisplayId, device);
+        			sendDisplayDeviceEventLocked(device, DISPLAY_DEVICE_EVENT_ADDED);
+        		} else if (device.updatePhysicalDisplayInfoLocked(mTempPhys)) {
+        			// Display properties changed.
+        			sendDisplayDeviceEventLocked(device, DISPLAY_DEVICE_EVENT_CHANGED);
+        		}
+        	} else {
+        		LocalDisplayDevice device = mDevices.get(builtInDisplayId);
+        		if (device != null) {
+        			// Display was removed.
+        			mDevices.remove(builtInDisplayId);
+        			sendDisplayDeviceEventLocked(device, DISPLAY_DEVICE_EVENT_REMOVED);
+            	}
+        	}
         }
     }
 
@@ -124,8 +142,23 @@ final class LocalDisplayAdapter extends DisplayAdapter {
         public DisplayDeviceInfo getDisplayDeviceInfoLocked() {
             if (mInfo == null) {
                 mInfo = new DisplayDeviceInfo();
+                if ((LocalDisplayAdapter.access$000() & 1) == 0) {
+                	if (LocalDisplayAdapter.access$100() != 1) {
                 mInfo.width = mPhys.width;
                 mInfo.height = mPhys.height;
+                	} else {
+                		mInfo.width = mPhys.height;
+                		mInfo.height = mPhys.width;
+                        }
+                	} else {
+                		if (LocalDisplayAdapter.access$100() != 1) {
+                			mInfo.width = mPhys.height;
+                    		mInfo.height = mPhys.width;
+                		} else {
+                			mInfo.width = mPhys.width;
+                            mInfo.height = mPhys.height;
+                		}
+                	}
                 mInfo.refreshRate = mPhys.refreshRate;
 
                 // Assume that all built-in displays that have secure output (eg. HDCP) also
@@ -145,6 +178,7 @@ final class LocalDisplayAdapter extends DisplayAdapter {
                     mInfo.xDpi = mPhys.xDpi;
                     mInfo.yDpi = mPhys.yDpi;
                     mInfo.touch = DisplayDeviceInfo.TOUCH_INTERNAL;
+                    mInfo.rotation = LocalDisplayAdapter.access$000();
                 } else {
                     mInfo.type = Display.TYPE_HDMI;
                     mInfo.name = getContext().getResources().getString(
