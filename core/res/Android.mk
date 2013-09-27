be0ae189bf4c497360ebc19b95a983ac1551892b
Boyd Mouritsen <boydmouritsen@Boyds-MacBook-Pro.local>
2013-09-27 13:09:56 -0700

    local merges

--- core/res/Android.mk
+++ core/res/Android.mk
@@ -22,7 +22,7 @@ LOCAL_CERTIFICATE := platform
 
 # Tell aapt to create "extending (non-application)" resource IDs,
 # since these resources will be used by many apps.
-LOCAL_AAPT_FLAGS := -x
+LOCAL_AAPT_FLAGS := -x --auto-add-overlay
 
 LOCAL_MODULE_TAGS := optional
 
